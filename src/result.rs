1c374f14a81e26c71a05f50f88b4b5f00d0bf258
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-08-28 12:26:21 -0300

    fix: implement std error

--- src/result.rs
+++ src/result.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "alloc")]
 use alloc::string::String;
 use core::array::TryFromSliceError;
+use core::fmt::Display;
+#[cfg(feature = "std")]
+use std::error::Error as StdError;
 #[cfg(feature = "std")]
 use std::io::Error as StdioError;
 
@@ -21,10 +24,16 @@ impl SQLiteError {
   }
 }
 
-#[cfg(feature = "std")]
-impl From<StdioError> for SQLiteError {
-  fn from(io_error: StdioError) -> Self {
-    Self::StdioError(io_error)
+impl Display for SQLiteError {
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    // TODO
+    write!(f, "{:?}", self)
+  }
+}
+
+impl From<TryFromSliceError> for SQLiteError {
+  fn from(error: TryFromSliceError) -> Self {
+    Self::TryFromSliceError(error)
   }
 }
 
@@ -35,8 +44,12 @@ impl From<String> for SQLiteError {
   }
 }
 
-impl From<TryFromSliceError> for SQLiteError {
-  fn from(error: TryFromSliceError) -> Self {
-    Self::TryFromSliceError(error)
+#[cfg(feature = "std")]
+impl StdError for SQLiteError {}
+
+#[cfg(feature = "std")]
+impl From<StdioError> for SQLiteError {
+  fn from(io_error: StdioError) -> Self {
+    Self::StdioError(io_error)
   }
 }
